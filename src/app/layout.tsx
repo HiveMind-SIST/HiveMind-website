f52870044880a3d499298de95745926a578165a2
Kishor Ramanan <hello@kishorramanan.com>
2025-09-22 20:38:00 +0530

    Fixed Dark Mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,8 +52,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
           disableTransitionOnChange
         >
           {children}
